ce74bc994d5d9ed04dfe8a32bf7946f9244f6543
fortydegrees <david@david-pratt.com>
2026-01-19 16:19:10 +0000

    feat: add travel sound for resource distribution

--- app/catana/effects/soundThemes.js
+++ app/catana/effects/soundThemes.js
@@ -1,5 +1,6 @@
 export const DEFAULT_THEME = {
   "resource:pop:start": { src: "/sounds/ui-pop-resource-out.mp3", volume: 0.6 },
+  "resource:travel:start": { src: "/sounds/card_woosh.mp3", volume: 0.6 },
   "dice:roll": { src: "/sounds/woosh-card.mp3", volume: 0.5, allowWhenHidden: true },
   "turn:start": { src: "/sounds/shimmer.mp3", volume: 0.6, allowWhenHidden: true }
 };
