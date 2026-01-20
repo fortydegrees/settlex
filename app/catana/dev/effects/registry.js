418d280fcede78979d8bbb2d010029f059ec18fd
fortydegrees <david@david-pratt.com>
2026-01-20 08:04:08 +0000

    feat: register piece placement lab

--- app/catana/dev/effects/registry.js
+++ app/catana/dev/effects/registry.js
@@ -1,6 +1,13 @@
 import { ResourceDistributionLab } from "./ResourceDistributionLab.jsx";
+import { PiecePlacementLab } from "./PiecePlacementLab.jsx";
 
 export const EFFECTS_LAB_REGISTRY = [
+  {
+    id: "piece-placement",
+    label: "Piece Placement",
+    component: PiecePlacementLab,
+    supportsAudio: true
+  },
   {
     id: "resource-distribution",
     label: "Resource Distribution",
