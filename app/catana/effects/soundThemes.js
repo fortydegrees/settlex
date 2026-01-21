c92f10807db65a0fca1540350229c0a6fb373aff
fortydegrees <david@david-pratt.com>
2026-01-21 10:25:27 +0000

    feat: map dice roll to shuffled audio variants

--- app/catana/effects/soundThemes.js
+++ app/catana/effects/soundThemes.js
@@ -4,6 +4,18 @@ export const DEFAULT_THEME = {
   "build:settlement": { src: "/sounds/settle.mp3", volume: 0.6 },
   "build:road": { src: "/sounds/road.mp3", volume: 0.6 },
   "build:place": { src: "/sounds/settle_place.mp3", volume: 0.6 },
-  "dice:roll": { src: "/sounds/woosh-card.mp3", volume: 0.5, allowWhenHidden: true },
+  "dice:roll": {
+    variants: [
+      "/sounds/dice_roll1.mp3",
+      "/sounds/dice_roll2.mp3",
+      "/sounds/dice_roll3.mp3",
+      "/sounds/dice_roll4.mp3",
+      "/sounds/dice_roll5.mp3"
+    ],
+    volume: 0.5,
+    allowWhenHidden: true,
+    shuffle: true,
+    randomize: { volume: [0.9, 1.0], rate: [0.98, 1.02] }
+  },
   "turn:start": { src: "/sounds/turn-start.mp3", volume: 0.6, allowWhenHidden: true }
 };
